d64a2c8ca8c32795b04777c38b7a29f4b06820d1
parmarpratham2006-lab <parmarpratham2006@gmail.com>
2026-08-13 12:56:52 +0530

    Add search function for rotated sorted array
    Implement binary search for rotated sorted array.

---
+++ daa assi 1/Searchinrotatedsearchedarray.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left =0, right = nums.length-1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[left]<= nums[mid]){ //it will help to check the target is inside the left sorted half
+                if(target>= nums[left] && target < nums[mid]){
+                    right =mid-1;
+                }
+                else{
+                    left =mid+1;
+                }
+            }
+            else{
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                }
+                else{
+                    right = mid-1;
+                }
+            }
+        } return -1;
+    }
+}
