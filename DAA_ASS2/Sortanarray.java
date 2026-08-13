8bb2df7414b5100fd13990fc174d2fa123f39b60
parmarpratham2006-lab <parmarpratham2006@gmail.com>
2026-08-13 13:14:13 +0530

    Implement merge sort algorithm in Sortanarray.java

---
+++ DAA_ASS2/Sortanarray.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums , 0, nums.length -1);
+        return nums;
+    }
+    private void mergeSort(int[] nums, int l, int r){
+        if(l>=r){
+            return;
+        }
+        int mid = l+(r-l)/2;
+        mergeSort(nums,l,mid);
+        mergeSort(nums, mid + 1, r);
+        merge(nums,l,mid,r);
+    }
+private void merge(int[] nums, int l, int mid, int r) {
+    int [] temp = new int[r-l+1];
+    int i = l,j = mid+1, k=0;
+    while(i<=mid && j<=r){
+        if(nums[i]<=nums[j]){
+            temp[k++] = nums[i++];
+        }else{
+            temp[k++] = nums[j++];
+        }
+    }
+    while(i<=mid){
+        temp[k++]=nums[i++];
+    }while(j<=r){
+        temp[k++] = nums[j++];
+    }for(int b = 0; b <temp.length; b++){
+        nums[l+b] = temp[b];
+    }
+}   
+}
